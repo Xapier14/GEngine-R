ae0bc31e5a86b730d25ec0f74f164eae4e265400
Lance Daniel Crisang <lancecrisang14@gmail.com>
2021-04-04 21:43:01 +0800

    Added audio loader.

--- src/Engine/Resource.cs
+++ src/Engine/Resource.cs
@@ -9,6 +9,7 @@ using System.Text.Json;
 
 using static SDL2.SDL;
 using static SDL2.SDL_image;
+using static SDL2.SDL_mixer;
 using System.Runtime.InteropServices;
 
 namespace GEngine.Engine
@@ -23,6 +24,7 @@ namespace GEngine.Engine
             _Audio = new ResourceCollection();
             _Textures = new ResourceCollection();
             IMG_Init(IMG_InitFlags.IMG_INIT_JPG | IMG_InitFlags.IMG_INIT_PNG | IMG_InitFlags.IMG_INIT_TIF);
+            Mix_Init(MIX_InitFlags.MIX_INIT_OGG | MIX_InitFlags.MIX_INIT_MP3 | MIX_InitFlags.MIX_INIT_OPUS | MIX_InitFlags.MIX_INIT_MID | MIX_InitFlags.MIX_INIT_FLAC);
             _SDL_Renderer = gameRenderer;
         }
         public void LoadAsTexture(string fileLocation, string resourceName)
@@ -80,6 +82,7 @@ namespace GEngine.Engine
             List<IntPtr> surfaces = new List<IntPtr>();
             List<IntPtr> textures = new List<IntPtr>();
             /* Load resource here */
+            SDL_GetError();
             foreach (string file in files)
             {
                 try
@@ -92,8 +95,18 @@ namespace GEngine.Engine
                     Debug.Log("ResourceManager.LoadAsTexture()", $"Read texture data from {file}#{resourceName} @ {byteData.Length} byte(s).");
                     IntPtr rwops = SDL_RWFromMem(handle.AddrOfPinnedObject(), byteData.Length);
                     IntPtr surface = IMG_Load_RW(rwops, 1);
+                    if (SDL_GetError() != "" || surface == IntPtr.Zero || rwops == IntPtr.Zero)
+                    {
+                        Debug.Log("ResourceManager.LoadAsTexture()", $"Error reading texture data {file}#{resourceName}.");
+                        continue;
+                    }
                     Debug.Log("ResourceManager.LoadAsTexture()", $"Loaded texture data as surface from {file}#{resourceName}.");
                     IntPtr texture = SDL_CreateTextureFromSurface(_SDL_Renderer, surface);
+                    if (SDL_GetError() != "" || texture == IntPtr.Zero)
+                    {
+                        Debug.Log("ResourceManager.LoadAsTexture()", $"Error creating texture data {file}#{resourceName}.");
+                        continue;
+                    }
                     surfaces.Add(surface);
                     textures.Add(texture);
                     Debug.Log("ResourceManager.LoadAsTexture()", $"Created texture from surface {file}#{resourceName}.");
@@ -116,6 +129,40 @@ namespace GEngine.Engine
             };
             _Textures.Add(res);
             Debug.Log("ResourceManager.LoadAsTexture()", $"Successfully added texture {resourceName} to resources.");
+        }
+        public void LoadAsAudio(string fileLocation, string resourceName, AudioType audioType)
+        {
+            if (_Audio.Contains(resourceName)) throw new ResourceException($"A resource with the same name '{resourceName}' already exists.", fileLocation);
+            if (!File.Exists(fileLocation)) throw new ResourceException($"Error loading resource '{resourceName}'. File not found.", fileLocation);
+            try
+            {
+                switch (audioType)
+                {
+                    default:
+                        throw new ResourceException("Audio type is invalid.", "ResourceManager.LoadAsAudio()");
+                    case AudioType.Music:
+                        IntPtr mus = Mix_LoadMUS(fileLocation);
+                        if (SDL_GetError() != "" || mus == IntPtr.Zero) throw new ResourceException($"Error loading '{fileLocation}'.","ResourceManager.LoadAsAudio()");
+                        AudioResource a1 = new AudioResource();
+                        a1.DataPtr = new IntPtr[1] { mus };
+                        a1.ResourceName = resourceName;
+                        a1.AudioType = AudioType.Music;
+                        _Audio.Add(a1);
+                        return;
+                    case AudioType.Effect:
+                        IntPtr eff = Mix_LoadWAV(fileLocation);
+                        if (SDL_GetError() != "" || eff == IntPtr.Zero) throw new ResourceException($"Error loading '{fileLocation}'.", "ResourceManager.LoadAsAudio()");
+                        AudioResource a2 = new AudioResource();
+                        a2.DataPtr = new IntPtr[1] { eff };
+                        a2.ResourceName = resourceName;
+                        a2.AudioType = AudioType.Effect;
+                        _Audio.Add(a2);
+                        return;
+                }
+            } catch
+            {
+                throw new ResourceException("Error in loading audio.", "ResourceManager.LoadAsAudio()");
+            }
         }
     }
     public class ResourceBase
